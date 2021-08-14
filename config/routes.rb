c95c95e7ed94a9c4770d67f8e21d5fcbd9620053
Marie-Pierre Jeffard <mariep.email@gmail.com>
2021-08-14 15:22:30 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [ :index, :show ] do
+    resources :teams, only: [ :new, :create, :index ]
+  end
+  resources :coworkings, only: [ :index, :show ] do
+    resources :bookings, only: [ :index, :new, :create ]
+  end
+  resources :bookings, only: [ :destroy ]
+  resources :teams, only: [ :destroy ]
 end
