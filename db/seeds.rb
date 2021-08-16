8115c46c57d7fba039157b970aee5323baf072b0
Marie-Pierre Jeffard <mariep.email@gmail.com>
2021-08-16 21:00:19 +0200

    seeds : WIP6 - random user wih avatar OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,38 +39,40 @@ abdlekrim = User.create!(
 abdlekrim.avatar.attach(io: File.open(Rails.root.join('app/assets/images/users/persona_02.jpg')), filename: 'persona_02.jpg')
 puts "#{abdlekrim.first_name} a été créé"
 
-# # Random Users —————————————————————————————————————————————————————————————————
+# Random Users —————————————————————————————————————————————————————————————————
 
-# def random_users
-#   url = "https://randomuser.me/api/?nat=fr"
-#   doc = URI.open(url).read
-#   json_parsed = JSON.parse(doc)
+def random_users
+  url = "https://randomuser.me/api/?nat=fr"
+  doc = URI.open(url).read
+  json_parsed = JSON.parse(doc)
 
-#   random_user = json_parsed["results"]
+  random_user = json_parsed["results"]
 
-#   job_titles = ["Chief happiness manager", "Administrateur réseau", "Traffic manager", "Country opener", "Développeur back-end", "Développeur full-stack", "Administrateur Système", "Responsable communication", "Business Developer", "Développeur", "Intégrateur web", "Community Manager", "Designer UX", "Directeur technique", "Business Developer", "Account Manager" ]
+  job_titles = ["Chief happiness manager", "Administrateur réseau", "Traffic manager", "Country opener", "Développeur back-end", "Développeur full-stack", "Administrateur Système", "Responsable communication", "Business Developer", "Développeur", "Intégrateur web", "Community Manager", "Designer UX", "Directeur technique", "Business Developer", "Account Manager" ]
 
-#   random_user.each do |attribute|
-#     attributes = {
-#       email: attribute["email"],
-#       password: "toto69",
-#       first_name: attribute["name"]["first"],
-#       last_name: attribute["name"]["last"],
-#       job_title: "#{job_titles.sample}",
-#       allowed_days_per_weeks: 2
-#     }
+  random_user.each do |attribute|
+    attributes = {
+      email: attribute["email"],
+      password: "toto69",
+      first_name: attribute["name"]["first"],
+      last_name: attribute["name"]["last"],
+      job_title: "#{job_titles.sample}",
+      allowed_days_per_weeks: 2,
+    }
+    random_avatar = open(attribute["picture"]["large"])
 
-#     user = User.create!(attributes)
-#     puts "#{user.first_name} a été créé"
-#   end
-# end
+    user = User.create!(attributes)
 
-# 3.times do
-#   random_users
-# end
+    user.avatar.attach(io: random_avatar, filename: 'avatar.jpg')
+    puts "#{user.first_name} a été créé"
+  end
+end
 
+20.times do
+  random_users
+end
 
-# puts "Création des teams..."
+puts "Création des teams..."
 
 puts "Et c'est finit !"
 
