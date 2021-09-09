e47263a43e55a5ce3f784d67e654814c800a37d4
Marie-Pierre Jeffard <mariep.email@gmail.com>
2021-09-09 16:14:50 +0200

    ajout des équipements dans la liste de coworkings

--- db/seeds/04_teams.rb
+++ db/seeds/04_teams.rb
@@ -1,12 +1,7 @@
 team_prod = Team.create!(
   name: "Team production",
-<<<<<<< HEAD
-  description: "équipe 2",
-  manager: User.find_by(email: "ma.bernard@company.com")
-=======
   description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit",
   # manager: User.find_by(email: "ma.bernard@company.com")
->>>>>>> 80127e6a194dcd2405807a16530de706fdbf3c04
 )
 
 User.last(5).each do |user|
