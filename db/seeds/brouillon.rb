619faa250a5035e170005a9adf170d7942e4aaa0
Marie-Pierre Jeffard <mariep.email@gmail.com>
2021-09-10 18:43:31 +0200

    coworking 05 crée

--- db/seeds/brouillon.rb
+++ db/seeds/brouillon.rb
@@ -40,3 +40,41 @@ CoworkingEquipement.create!({ coworking_id: coworking.id, equipement_id: secu_do
 puts "#{walter_technolac.name} a été créé"
 
 
+# ******************************************************************************
+# Coworking 05 —————————————————————————————————————————————————————————————————
+
+luz_in = Coworking.create!(
+  name: "LUZ'IN",
+  address: "PA de Rhodes - 3 rue des Mûriers 38110 La Tour du Pin",
+  opening_time: "08:30",
+  closing_time: "18:00",
+  contact_name: "Julia Sorlin"
+)
+
+# Photos ———————————————————————————————————————————————————————————————————————
+
+luz_in_photos = [
+  'https://res.cloudinary.com/mapiemap/image/upload/v1631289906/Extramuros/Coworkings/luz-in-01_fwh0i3.jpg',
+  'https://res.cloudinary.com/mapiemap/image/upload/v1631289906/Extramuros/Coworkings/luz-in-02_li9g0l.jpg',
+  'https://res.cloudinary.com/mapiemap/image/upload/v1631289906/Extramuros/Coworkings/luz-in-03_qb4udd.jpg'
+]
+luz_in_photo_number = 0
+luz_in_photos.each do |photo|
+  file = URI.open(photo)
+  luz_in.photos.attach(io: file, filename: "luz-in-0#{luz_in_photo_number += 1}.jpg")
+end
+
+luz_in_avatar = URI.open('https://res.cloudinary.com/mapiemap/image/upload/v1631289906/Extramuros/Coworkings/luz-in-avatar_p4eivm.jpg')
+luz_in.avatar.attach(io: luz_in_avatar, filename: 'luz-in-avatar.jpg')
+
+
+coworking = Coworking.find_by(name: "LUZ'IN")
+
+CoworkingEquipement.create!({ coworking_id: coworking.id, equipement_id: parking.id })
+CoworkingEquipement.create!({ coworking_id: coworking.id, equipement_id: lunch.id })
+CoworkingEquipement.create!({ coworking_id: coworking.id, equipement_id: printer.id })
+CoworkingEquipement.create!({ coworking_id: coworking.id, equipement_id: wifi.id })
+CoworkingEquipement.create!({ coworking_id: coworking.id, equipement_id: access.id })
+
+puts "#{luz_in.name} a été créé"
+
