8ae40a744520952f05586e1662f9838fa710bc81
Marie-Pierre Jeffard <79690707+MapieMaps@users.noreply.github.com>
2021-09-16 17:46:09 +0200

    seeds testées (#28)

---
+++ db/seeds/06_bookings.rb
@@ -0,0 +1,30 @@
+# Past Bokkings
+
+def random_past_booking
+  rand_number = rand(1...8)
+  Booking.create!(
+    user_id: User.find_by(email: "ma.bernard@company.com").id,
+    coworking_id: Coworking.find_by(name: "Work & Wall").id,
+    date: "2021-0#{rand_number}-03"
+  )
+end
+
+12.times do
+  random_past_booking
+end
+
+# Futur Bookings
+
+Booking.create!(
+  user_id: User.find_by(email: "ma.bernard@company.com").id,
+  coworking_id: Coworking.find_by(name: "ChloroFEEL").id,
+  date: "2021-09-23"
+)
+
+Booking.create!(
+  user_id: User.find_by(email: "ma.bernard@company.com").id,
+  coworking_id: Coworking.find_by(name: "Work & Wall").id,
+  date: "2021-09-30"
+)
+
+puts "Les résas de Marie-Ange ont été créé"
