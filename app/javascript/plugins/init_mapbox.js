e6f7abb599afecddd91bc734bed369bc67764ade
Marie-Pierre Jeffard <mariep.email@gmail.com>
2021-09-16 19:22:10 +0200

    retour à un style de map standard

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,8 +5,11 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    // style: 'mapbox://styles/mapbox/streets-v10'
-    style: 'mapbox://styles/mapiemaps/cktke699q823417qa649323o4'
+    // style: 'mapbox://styles/mapbox/streets-v10' // Style par defaut
+    // Mon style >> mapbox://styles/mapiemaps/cktke699q823417qa649323o4
+
+    style: 'mapbox://styles/mapbox/light-v10'
+
   });
 };
 
@@ -18,11 +21,13 @@ const addMarkersToMap = (map, markers) => {
   });
 };
 
+
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 200, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 150, maxZoom: 15, duration: 0 });
 };
+
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
